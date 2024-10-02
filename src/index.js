eb41cae8745cfa88ce87cf51c15ed6046081f543
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-02 19:51:03 +0530

    Add utilities functions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
-
-/* 
-    We can define the database connection   function here too like the one given below, but 
-    for code modularity, we do that in the db/index file
-    
-    Also, for DB connection, We could have used traditional functions but we choose IIFE so 
-    that this connection to the database is executed immediately.
-
-    ;(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        app.on("error", (error) => {
-        console.log("Error:", error);
-        });
-        app.listen(process.env.PORT, () => {
-        console.log(`App is listening on ${process.env.PORT}`);
-        });
-    } catch (err) {
-        console.error("ERROR: ", err);
-        throw err;
-    }
-    })();
-*/
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 5173, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection fail ", err);
+  });
